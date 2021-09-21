ebe25f8a4b70a50bc7a9a3fbfaff170c642d384e
SusmitDeshpande <satejsdeshpande@gmail.com>
2021-09-21 15:16:08 +0530

    update api's for flowers and cakes added

--- server/controllers/flower.js
+++ server/controllers/flower.js
@@ -1,4 +1,5 @@
 import FlowerModel from "../models/flower.js"
+import mongoose from 'mongoose';
 
 export const getAllFlowers = async (req,res) => {
     try {
@@ -19,4 +20,17 @@ export const addFlower = async (req,res) => {
     } catch (error) {
         res.status(409).json({message: error.message});
     }
+}
+
+export const updateFlower = async (req, res) => {
+    const {id:_id} = req.params;
+    const updFlower = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(_id)){
+        return res.status(404).send("No Cakes with this ID.");
+    }
+
+    const updatedFlower = await FlowerModel.findByIdAndUpdate(_id, updFlower, {new:true})
+    res.json(updatedFlower);
+
 }
--- server/routes/cakes.js
+++ server/routes/cakes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import {getAllCakes, addCake} from '../controllers/cake.js'
+import {getAllCakes, addCake, updateCake} from '../controllers/cake.js'
 
 const router = express.Router();
 
 router.get('/', getAllCakes);
 router.post('/addCake', addCake);
+router.patch('/:id', updateCake)
 
 export default router;
