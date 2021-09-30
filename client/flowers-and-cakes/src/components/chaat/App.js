40562ccf75b54b7c715ac18506fbaa3c360e3dbf
Interimuser <Interimuser@HJNZL001338.persistent.co.in>
2021-09-30 21:41:57 +0530

    chatbot daal diya

---
+++ client/flowers-and-cakes/src/components/chaat/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import SimpleForm from "./components/chatbots/SimpleForm";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SimpleForm/>
+      </div>
+    );
+  }
+       
+}
+
+export default App;
